115f6c5512e9168060cd53fc3cdea42ecc086ad4
menezes0067 <menezes0067@gmail.com>
2026-04-30 12:38:05 -0300

    refactor: gaming.nix

--- extras/gaming/gaming.nix
+++ extras/gaming/gaming.nix
@@ -7,9 +7,6 @@
         enable = true;
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
-        gamescopeSession = {
-          enable = true;
-        };
         
         extraCompatPackages = with pkgs; [
           proton-ge-bin
